af7d90fac6a985dc36b5c2146608f0b78fc9accd
pdmarkovits <panagiotis.markovits@athenarc.gr>
2024-07-26 02:26:22 +0300

    - Added inferred metadata to DOI mode response

--- src/fos/server/gradio_app.py
+++ src/fos/server/gradio_app.py
@@ -8,15 +8,16 @@ This gradio app will be hosted on HF spaces for demo purposes.
 
 import os
 import gradio as gr
+import json
 import requests as req
 from fos.pipeline.inference import create_payload, infer, process_pred
 
-# Retrieve HF space secrets
+# retrieve HF space secrets
 BACKEND_IP = os.getenv('BACKEND_IP')
 BACKEND_PORT = os.getenv('BACKEND_PORT')
 BACKEND_PATH = os.getenv('BACKEND_PATH')
 
-# Define the functions to handle the inputs and outputs
+# define the functions to handle the inputs and outputs
 def analyze_input(
     my_id: str | None,
     title: str | None,
@@ -60,23 +61,33 @@ def analyze_input(
 def analyze_input_doi(
     doi: str | None
 ):
-    if (doi is None):
-        results = {'error': 'Please provide the DOI of the publication'}
-        return results
-    if (doi == ''):
+    if doi is None or doi == '':
         results = {'error': 'Please provide the DOI of the publication'}
         return results
     try:
         url = f"http://{BACKEND_IP}:{BACKEND_PORT}{BACKEND_PATH}{doi}"
         response = req.get(url)
         response.raise_for_status()
-        results = response.json()
+        doi_results = response.json()
+        # parse response to call analyze_input
+        title = doi_results.get("title", "")
+        abstract = doi_results.get("abstract", "")
+        pub_venue = doi_results.get("pub_venue", "")
+        ref_venues = ", ".join(doi_results.get("ref_venues", []))
+        cit_venues = ", ".join(doi_results.get("cit_venues", []))
+        metadata_results = analyze_input(doi, title, abstract, pub_venue, ref_venues, cit_venues)
+        # combine the two outputs
+        combined_results = {
+            "inferred_metadata": doi_results,
+            "results": metadata_results.get("data", "")
+        }
+        results = json.dumps(combined_results)
         return results
     except Exception as e:
         results = {'error': str(e)}
     return results
 
-# Define the interface for the first tab (Text Analysis)
+# define the interface for the first tab (Text Analysis)
 with gr.Blocks() as text_analysis:
     gr.Markdown("### SciNoBo Field of Science (FoS) Classification - Metadata Mode")
     id_input = gr.Textbox(label="ID (e.g. DOI)", placeholder="Enter an ID for the publication. At this demo, it is only used for reference.")
@@ -84,10 +95,10 @@ with gr.Blocks() as text_analysis:
     abstract_input = gr.Textbox(label="Abstract", placeholder="Enter the abstract of the publication")
     pub_venue_input = gr.Textbox(label="Publication Venue", placeholder="Enter the publication venue that the publication was published")
     ref_venues_input = gr.Textbox(label="Reference Venues", placeholder="Enter the venues that the publication references, separated by commas")
-    cit_veunes_input = gr.Textbox(label="Citation Venues", placeholder="Enter the venues that cite the publication, separated by commas")
+    cit_venues_input = gr.Textbox(label="Citation Venues", placeholder="Enter the venues that cite the publication, separated by commas")
     process_text_button = gr.Button("Process")
     text_output = gr.JSON(label="Output")
-    process_text_button.click(analyze_input, inputs=[id_input, title_input, abstract_input, pub_venue_input, ref_venues_input, cit_veunes_input], outputs=[text_output])
+    process_text_button.click(analyze_input, inputs=[id_input, title_input, abstract_input, pub_venue_input, ref_venues_input, cit_venues_input], outputs=[text_output])
     examples = gr.Examples(
         [[
             "10.18653/v1/w19-5032",
@@ -97,10 +108,10 @@ with gr.Blocks() as text_analysis:
             "acl,acl,aimag,arxiv artificial intelligence,arxiv computation and language,arxiv machine learning,arxiv social and information networks,briefings in bioinformatics,comparative and functional genomics,conference of the european chapter of the association for computational linguistics,cvpr,emnlp,emnlp,emnlp,emnlp,emnlp,emnlp,emnlp,eswc,iclr,icml,kdd,kdd,kdd,kdd",
             "naacl,nips,nucleic acids res,pacific symposium on biocomputing,physica a statistical mechanics and its applications,proceedings of the acm conference on bioinformatics computational biology and health informatics,sci china ser f,the web conference"
         ]],
-        inputs=[id_input, title_input, abstract_input, pub_venue_input, ref_venues_input, cit_veunes_input]
+        inputs=[id_input, title_input, abstract_input, pub_venue_input, ref_venues_input, cit_venues_input]
     )
 
-# Define the interface for the second tab (DOI Mode)
+# define the interface for the second tab (DOI Mode)
 with gr.Blocks() as doi_mode:
     gr.Markdown("### SciNoBo Field of Science (FoS) Classification - DOI Mode")
     doi_input = gr.Textbox(label="DOI", placeholder="Enter a valid Digital Object Identifier")
@@ -108,9 +119,9 @@ with gr.Blocks() as doi_mode:
     doi_output = gr.JSON(label="Output")
     process_doi_button.click(analyze_input_doi, inputs=[doi_input], outputs=[doi_output])
 
-# Combine the tabs into one interface
+# combine the tabs into one interface
 with gr.Blocks() as demo:
     gr.TabbedInterface([text_analysis, doi_mode], ["Metadata Mode", "DOI Mode"])
 
-# Launch the interface
+# launch the interface
 demo.queue().launch(server_name="0.0.0.0", server_port=7860)
